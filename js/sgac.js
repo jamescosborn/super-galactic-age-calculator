85a34b486d3899f722836e3a942eca9108ba05da
James Osborn <jamescarlosborn@gmail.com>
2017-12-05 22:17:35 -0800

    test and method for seconds between 2 dates, beginning of life expectencies.

--- js/sgac.js
+++ js/sgac.js
@@ -1,30 +1,54 @@
 export class Age {
- constructor(yearsOld) {
-   this.age = yearsOld;
- }
+  constructor(yearsOld) {
+    this.age = yearsOld;
+  }
 
- ageToSeconds() {
-   let newAge = this.age * 31536000;
-   return newAge;
- }
+  ageToSeconds() {
+    let newAge = this.age * 31536000;
+    return newAge;
+    }
 
- mercuryRising() {
-   let newAge = this.age * .24;
-   return newAge;
- }
+  earthYearsLeft() {
+    let yearsLeft = 79 - this.age;
+    return yearsLeft;
+  }
 
- venusAge() {
-   let newAge = this.age * .62;
-   return newAge;
- }
+  mercuryRising() {
+    let newAge = this.age * .24;
+    return newAge;
+    }
 
- marsAge() {
-   let newAge = this.age * 1.88;
-   return newAge;
- }
+  mercuryYearsLeft() {
+    let newAge = this.age * .24;
+    let expectancy = 79 * .24;
+    let yearsLeft = expectancy - newAge;
+    return yearsLeft;
+  }
 
- jupiterAge() {
-   let newAge = this.age * 11.86;
-   return newAge;
- }
+  venusAge() {
+    let newAge = this.age * .62;
+    return newAge;
+    }
+
+  marsAge() {
+    let newAge = this.age * 1.88;
+    return newAge;
+    }
+
+  jupiterAge() {
+    let newAge = this.age * 11.86;
+    return newAge;
+    }
+}
+
+export class BirthYear {
+  constructor(birthyear) {
+    this.birthyear = birthyear;
+  }
+
+  bornSecondsAgo() {
+    let newAge = 2017 - this.birthyear;
+    let secondsOld = newAge * 31536000;
+    return secondsOld;
+  }
 }
--- spec/sgac-spec.js
+++ spec/sgac-spec.js
@@ -1,4 +1,5 @@
 import { Age } from "./../js/sgac.js";
+import { BirthYear } from "./../js/sgac.js";
 
 describe('ageToSeconds', function() {
   it('should turn age in years into seconds', function () {
@@ -32,5 +33,26 @@ describe('jupiterAge', function() {
   it('should turn Earth years into Jupiter years', function () {
     const age = new Age(7);
     expect(age.jupiterAge()).toEqual(83.02)
+  })
+})
+
+describe('differenceInSeconds', function() {
+  it('should determine the seconds between one years and another', function () {
+    const birthdate = new BirthYear(2015);
+    expect(birthdate.bornSecondsAgo()).toEqual(63072000);
+  })
+})
+
+describe('earthYearsLeft', function() {
+  it('should determine years left on Earth', function () {
+    const age = new Age(7);
+    expect(age.earthYearsLeft()).toEqual(72)
+  })
+})
+
+describe('mercuryYearsLeft', function() {
+  it('should determine years left on Mercury', function () {
+    const age = new Age(7);
+    expect(age.mercuryYearsLeft()).toEqual(17.28)
   })
 })
